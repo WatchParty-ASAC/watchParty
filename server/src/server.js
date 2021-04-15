34dbc702d2db090d96f3251aaea19aa8cb424008
afnandamra <damra.afnan@gmail.com>
2021-04-15 20:57:49 +0300

    added files

--- server/src/server.js
+++ server/src/server.js
@@ -25,6 +25,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '..', '..', 'client', 'public')));
+// console.log(path.join(__dirname,'..', 'public'));
 // app.use(express.static('../client/public'));
 
 // ----------- //

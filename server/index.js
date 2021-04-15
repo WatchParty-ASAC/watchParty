c60b2d87be0964b8f3f7571365c456156eebf08d
afnandamra <damra.afnan@gmail.com>
2021-04-15 20:51:47 +0300

    added files

--- server/index.js
+++ server/index.js
@@ -1,13 +1,4 @@
 'use strict';
 
 require('dotenv').config();
-
-const mongoose = require('mongoose');
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-};
-mongoose.connect(process.env.MONGODB_URI, options);
-
 require('./src/server').start(process.env.PORT)
--- server/src/server.js
+++ server/src/server.js
@@ -1,42 +1,100 @@
 'use strict';
+
+// require('dotenv').config();
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 const cors = require('cors');
 const morgan = require('morgan');
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
-  cors: {
-    origin: '*',
-  },
-});
-io.listen(server);
+const path = require('path');
+const socket = require('socket.io');
 
-// Esoteric Resources
+const io = socket(server, {
+  cors: { origin: '*' },
+  // transports: ['websocket', 'polling'],
+});
+
+const { userJoin, getUsers, userLeave, currentUser } = require('./utilis/user');
+const messageTemplate = require('./utilis/messages');
+
 const errorHandler = require('./errors-handlers/500');
 const notFound = require('./errors-handlers/404');
-const authRoutes = require('./auth/router');
-const bearerAuth = require('./auth/middleware/bearerAuth');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan('dev'));
-app.use(express.static('/client'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..', '..', 'client', 'public')));
+// app.use(express.static('../client/public'));
 
-app.use(authRoutes);
+// ----------- //
+// io.listen(server);
 
-app.get('/', bearerAuth, homePage);
+io.on('connection', (socket) => {
+  console.log(socket.id);
+  //join room
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-function homePage(req, res) {
-  res.send('Home Page');
-}
+    socket.join(user.room);
+
+    //welcome for user
+    socket.emit(
+      'message',
+      messageTemplate(user.username, `welcome to ${user.room}`)
+    );
+
+    //broadcast to all member
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        messageTemplate(user.username, `${user.username} has joined`)
+      );
+
+    //room info /// users
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getUsers(user.room),
+    });
+  });
+
+  socket.on('chatMessage', (msg) => {
+    const user = currentUser(socket.id);
+
+    io.to(user.room).emit('message', messageTemplate(user.username, msg));
+  });
+
+  socket.on('videoUrl', (videoUrl) => {
+    const user = currentUser(socket.id);
+    io.to(user.room).emit('video', videoUrl);
+  });
+
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        messageTemplate(user.username, `${user.username} has left`)
+      );
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getUsers(user.room),
+      });
+    }
+
+  });
+});
+
+// ----------- //
 
 // Catchalls
 app.use(notFound);
 app.use(errorHandler);
 
 module.exports = {
-  io,
+  // io,
   server: app,
   start: (port) => {
     if (!port) {

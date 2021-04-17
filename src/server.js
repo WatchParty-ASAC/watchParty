b17f2dffd68897efe3690f948f76feb77b362b46
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-04-18 01:38:08 +0300

    fixed a bug

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../../client/public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 let likes = 0;
 

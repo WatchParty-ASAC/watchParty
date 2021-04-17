1f2b35240704e0209a17f68a3d00b34eb6a4afe9
Boshra Jaber <boshrajaber94@gmail.com>
2021-04-17 20:12:55 +0300

    like btn working

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
 const socket = require('socket.io');
+var counter = 8;
 
 const io = socket(server, {
 	cors: { origin: '*' },
@@ -52,6 +53,7 @@ io.on('connection', (socket) => {
 			room: user.room,
 			users: getUsers(user.room),
 		});
+
 	});
 
 	socket.on('chatMessage', (msg) => {
@@ -63,6 +65,14 @@ io.on('connection', (socket) => {
 	socket.on('videoUrl', (videoUrl) => {
 		const user = currentUser(socket.id);
 		io.to(user.room).emit('video', videoUrl);
+		//Like Btn
+		socket.emit('click_count', counter);
+
+		socket.on('clicked', function () {
+			counter += 1;
+
+			socket.emit('click_count', counter);
+		});
 	});
 
 	socket.on('disconnect', () => {
@@ -80,6 +90,7 @@ io.on('connection', (socket) => {
 		}
 	});
 });
+
 
 // ----------- //
 
